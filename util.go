08a1ca10e8e3635a730a34bf75f6654681e85ea9
Mathew-Estafanous <mathewestafanous13@gmail.com>
2021-10-31 21:58:13 -0400

    Move utility methods to a separate util file.

---
+++ util.go
@@ -0,0 +1,50 @@
+package memlist
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+// remove the element from the string slice at the given index.
+func remove(s []string, i int) []string {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+// insert a string into the slice with the given value and at the index.
+func insert(a []string, index int, value string) []string {
+	if len(a) == index { // nil or empty slice or after last element
+		return append(a, value)
+	}
+	a = append(a[:index+1], a[index:]...) // index < len(a)
+	a[index] = value
+	return a
+}
+
+// encodeMessage will encode the provided type into a byte slice and appends the
+// message type to the front of the byte slice.
+func encodeMessage(tp messageType, e interface{}) ([]byte, error) {
+	buf := bytes.NewBuffer([]byte{uint8(tp)})
+	enc := gob.NewEncoder(buf)
+	if err := enc.Encode(e); err != nil {
+		return nil, err
+	}
+	packetSize := i32tob(uint32(buf.Len()))
+	return append(packetSize, buf.Bytes()...), nil
+}
+
+func i32tob(val uint32) []byte {
+	r := make([]byte, 4)
+	for i := uint32(0); i < 4; i++ {
+		r[i] = byte((val >> (8 * i)) & 0xff)
+	}
+	return r
+}
+
+func btoi32(val []byte) uint32 {
+	r := uint32(0)
+	for i := uint32(0); i < 4; i++ {
+		r |= uint32(val[i]) << (8 * i)
+	}
+	return r
+}
